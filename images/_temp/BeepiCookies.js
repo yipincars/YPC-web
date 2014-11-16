306b3fb35bb8b2950b4fd52d3bd92ff3906b4385
catee <452416115@qq.com>
2014-11-16 18:54:56 +0800

    web new code

---
+++ images/_temp/BeepiCookies.js
@@ -0,0 +1,18 @@
+﻿function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var allCookies= document.cookie.split(';');
+    for (var i = 0; i < allCookies.length; i++) {
+        var currCookie = allCookies[i].trim();
+        if (currCookie.indexOf(name) == 0)
+            return currCookie.substring(name.length, currCookie.length);
+    }
+
+    return null;
+}
+
+function setCookie(cookieName, cookieValue, expMinutes) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expMinutes * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cookieName + "=" + cookieValue + "; " + expires;
+}
